bb3881bbc1f1c32b3815cdda988110c13defb460
Ewa Manek and Sheena Nickerson <ewa@>
2016-08-16 14:12:24 -0700

    Replace for loop with recursive loop because we are ambitious

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,21 @@ function createFactorial(number) {
   if(number === 0) {
     return 1;
   } else {
-    var factorial = 1;
-    for (var i = 1; i <= number; i++) {
-      factorial *= i;
-    } //end of for loop
-    return factorial;
+    //with for loop
+    // var factorial = 1;
+    // for (var i = 1; i <= number; i++) {
+    //   factorial *= i;
+    // } //end of for loop
+
+    //with recursion
+    if(number > 1){
+      //console.log("number = " + number);
+      var number2 = createFactorial(number-1);
+      //console.log("number = " + number + " number2 = " + number2);
+      return number * number2;
+    }
+    //console.log("number = " + number);
+    return number;
   } //end of else
 } // end of function
 
