0e224ae0f4a987bd7ee0de27fdda9b725bae549e
Ewa Manek and Sheena Nickerson <sheena@>
2016-08-16 13:47:41 -0700

    Add logic to calculate factorials

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 //<!-- Back End -->
 
+function createFactorial(number) {
+  if(number === 0) {
+    return 1;
+  } else {
+    var factorial = 1;
+    for (var i = 1; i <= number; i++) {
+      factorial *= i;
+    } //end of for loop
+    return factorial;
+  } //end of else
+} // end of function
+
 
 //<!-- Front End  -->
 $(document).ready(function(){
-
+ $("form").submit(function(event) {
+   event.preventDefault();
+   var number = parseInt($("input#toFactorial").val());
+   $(".well").text(createFactorial(number));
+ });
 
 });
